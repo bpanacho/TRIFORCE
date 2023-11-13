7ff0f7bc2f80e6d474f39de86fa7378f2f7506a0
Pedro Campos <149902237+JFcamp@users.noreply.github.com>
2023-11-13 01:33:25 -0300

    Create verificar_PKL.py
    Esse arquivo exibe as coordenadas de cada um dos campos já existentes e suas devidas respostas

---
+++ verificar_PKL.py
@@ -0,0 +1,13 @@
+import pickle
+
+# Para abrir o arquivo campos.pkl
+with open('C:\\Users\\pedro\\Downloads\\formulario-animado-main (2)\\formulario-animado-main\\campos.pkl', 'rb') as arquivo_campos:
+    campos = pickle.load(arquivo_campos)
+
+# Para abrir o arquivo resp.pkl
+with open('C:\\Users\\pedro\\Downloads\\formulario-animado-main (2)\\formulario-animado-main\\resp.pkl', 'rb') as arquivo_respostas:
+    respostas = pickle.load(arquivo_respostas)
+
+# Agora, você pode acessar as variáveis campos e respostas que contêm os dados desses arquivos
+print("Campos:", campos)
+print("Respostas:", respostas)
