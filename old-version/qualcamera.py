317b0827c2a5ebd32c514187ad433926625605be
JFcamp <pedrocampos2749@gmail.com>
2023-11-14 20:06:32 -0300

    slide triforce

--- old-version/qualcamera.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-for i in range(10):
-    camera = cv2.VideoCapture(i)
-    if not camera.isOpened():
-        break
-    print(f"Câmera {i}: {camera.getBackendName()}")
-    camera.release()
